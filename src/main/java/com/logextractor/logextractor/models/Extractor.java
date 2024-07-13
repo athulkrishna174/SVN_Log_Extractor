dff6f62e423379d8a2b59d43d3390b7f08997c81
Athul Krishna P S <athulkrishna174@gmail.com>
2024-07-13 18:02:12 +0530

    removed lombok plugin

--- src/main/java/com/logextractor/logextractor/models/Extractor.java
+++ src/main/java/com/logextractor/logextractor/models/Extractor.java
@@ -5,15 +5,6 @@ import java.util.List;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.web.multipart.MultipartFile;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Extractor {
 
 	private MultipartFile file;
@@ -26,5 +17,81 @@ public class Extractor {
 	private Workbook workbook;
 
 	private String message;
+
+	public Extractor() {
+		super();
+	}
+
+	public MultipartFile getFile() {
+		return file;
+	}
+
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+
+	public String getOutputFileFormat() {
+		return outputFileFormat;
+	}
+
+	public void setOutputFileFormat(String outputFileFormat) {
+		this.outputFileFormat = outputFileFormat;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public List<String> getSelectedProjects() {
+		return selectedProjects;
+	}
+
+	public void setSelectedProjects(List<String> selectedProjects) {
+		this.selectedProjects = selectedProjects;
+	}
+
+	public List<String> getOptionList() {
+		return optionList;
+	}
+
+	public void setOptionList(List<String> optionList) {
+		this.optionList = optionList;
+	}
+
+	public List<String> getRequiredFormats() {
+		return requiredFormats;
+	}
+
+	public void setRequiredFormats(List<String> requiredFormats) {
+		this.requiredFormats = requiredFormats;
+	}
+
+	public String getExtractedString() {
+		return extractedString;
+	}
+
+	public void setExtractedString(String extractedString) {
+		this.extractedString = extractedString;
+	}
+
+	public Workbook getWorkbook() {
+		return workbook;
+	}
+
+	public void setWorkbook(Workbook workbook) {
+		this.workbook = workbook;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
 }
--- src/main/java/com/logextractor/logextractor/models/ExtractorDto.java
+++ src/main/java/com/logextractor/logextractor/models/ExtractorDto.java
@@ -4,15 +4,6 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class ExtractorDto {
 	private MultipartFile file;
 	private String outputFileFormat;
@@ -20,4 +11,68 @@ public class ExtractorDto {
 	private List<String> selectedProjects;
 	private List<String> optionList;
 	private List<String> requiredFormats;
+	
+	public ExtractorDto() {
+		super();
+	}
+
+	public ExtractorDto(MultipartFile file, String outputFileFormat, String projectName, List<String> selectedProjects,
+			List<String> optionList, List<String> requiredFormats) {
+		super();
+		this.file = file;
+		this.outputFileFormat = outputFileFormat;
+		this.projectName = projectName;
+		this.selectedProjects = selectedProjects;
+		this.optionList = optionList;
+		this.requiredFormats = requiredFormats;
+	}
+
+	public MultipartFile getFile() {
+		return file;
+	}
+
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+
+	public String getOutputFileFormat() {
+		return outputFileFormat;
+	}
+
+	public void setOutputFileFormat(String outputFileFormat) {
+		this.outputFileFormat = outputFileFormat;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public List<String> getSelectedProjects() {
+		return selectedProjects;
+	}
+
+	public void setSelectedProjects(List<String> selectedProjects) {
+		this.selectedProjects = selectedProjects;
+	}
+
+	public List<String> getOptionList() {
+		return optionList;
+	}
+
+	public void setOptionList(List<String> optionList) {
+		this.optionList = optionList;
+	}
+
+	public List<String> getRequiredFormats() {
+		return requiredFormats;
+	}
+
+	public void setRequiredFormats(List<String> requiredFormats) {
+		this.requiredFormats = requiredFormats;
+	}
+	
 }
